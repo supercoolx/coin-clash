fe3f7319a49ddc360c9415cd889bf47fe2dad067
supercoolx <94512319+supercoolx@users.noreply.github.com>
2024-10-28 01:04:12 -0700

    Fix form and dark chart

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,34 +1,34 @@
 const Create = () => {
   return (
-    <div className="container mx-auto flex flex-col items-center pb-24 px-4">
+    <div className="container flex flex-col items-center px-4 pb-24 mx-auto">
       <form action="#" className="space-y-6 mt-10 w-full max-w-[400px]">
         <div className="flex flex-col gap-2">
           <label htmlFor="name" className="font-bold">Token Name</label>
-          <input type="text" name="name" id="name" className="rounded-lg border border-white outline-none bg-slate-800 px-2 py-2 w-full" />
+          <input type="text" name="name" id="name" className="w-full px-2 py-2 border border-white rounded-lg outline-none bg-slate-800" />
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="ticker" className="font-bold">Ticker ($)</label>
-          <input type="text" name="name" id="ticker" className="rounded-lg border border-white outline-none bg-slate-800 px-2 py-2 w-full" />
+          <input type="text" name="name" id="ticker" className="w-full px-2 py-2 border border-white rounded-lg outline-none bg-slate-800" />
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="desc" className="font-bold">Description</label>
-          <input type="text" name="name" id="desc" className="rounded-lg border border-white outline-none bg-slate-800 px-2 py-2 w-full" />
+          <input type="text" name="name" id="desc" className="w-full px-2 py-2 border border-white rounded-lg outline-none bg-slate-800" />
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="thumbnail" className="font-bold">Image or Video</label>
-          <input type="text" name="name" id="thumbnail" className="rounded-lg border border-white outline-none bg-slate-800 px-2 py-2 w-full" />
+          <input type="text" name="name" id="thumbnail" className="w-full px-2 py-2 border border-white rounded-lg outline-none bg-slate-800" />
         </div>
-        <div className="bg-dark-gray p-4 pr-6 rounded-3xl">
+        <div className="p-4 pr-6 bg-dark-gray rounded-3xl">
           <div className="flex gap-4">
             <img src="/imgs/logo.webp" alt="" className="w-16 h-16 rounded-full" />
             <div className="">
               <div className="text-lg font-semibold">SuperDawhg</div>
-              <div className="text-neutral-500 font-semibold">The best dog on CoinClash</div>
+              <div className="font-semibold text-neutral-500">The best dog on CoinClash</div>
             </div>
           </div>
         </div>
         <div className="flex justify-center">
-          <button className="rounded-full bg-primary flex items-center justify-center text-black text-sm h-8 transition-all duration-300 font-semibold hover:bg-secondary hover:text-white w-80">Create a new coin</button>
+          <button type="button" className="flex items-center justify-center h-8 text-sm font-semibold text-black transition-all duration-300 rounded-full bg-primary hover:bg-secondary hover:text-white w-80">Create a new coin</button>
         </div>
       </form>
     </div>
--- src/pages/Trade.jsx
+++ src/pages/Trade.jsx
@@ -19,7 +19,7 @@ const Trade = () => {
       container_id: "tradingview_bnbusdt",
       symbol: "BINANCE:BNBUSDT",
       interval: "D",
-      theme: "light",
+      theme: "dark",
       style: "1",
       locale: "en",
       toolbar_bg: "#f1f3f6",
@@ -40,50 +40,50 @@ const Trade = () => {
         <div id="tradingview_bnbusdt" style={{ height: "600px", width: "100%" }} />
       </div>
       <div className="">
-        <div className="bg-dark-gray p-4 pr-6 rounded-3xl">
+        <div className="p-4 pr-6 bg-dark-gray rounded-3xl">
           <div className="flex gap-4">
             <img src="/imgs/logo.webp" alt="" className="w-16 h-16 rounded-full" />
             <div className="">
               <div className="text-lg font-semibold">SuperDawhg (SDOG)</div>
-              <div className="text-neutral-500 font-semibold">The best dog on CoinClash</div>
+              <div className="font-semibold text-neutral-500">The best dog on CoinClash</div>
             </div>
-            <div className="text-primary font-bold ml-8">#1</div>
+            <div className="ml-8 font-bold text-primary">#1</div>
           </div>
         </div>
-        <div className="rounded-full mt-2 px-4 flex justify-between items-center py-2 font-bold bg-dark-gray">
+        <div className="flex items-center justify-between px-4 py-2 mt-2 font-bold rounded-full bg-dark-gray">
           <div className="text-primary">market cap: 2.15M</div>
           <div className="flex items-center gap-1">
             <img src="/imgs/user.svg" alt="" className="w-4 h-4" />
             <span>12.508</span>
           </div>
         </div>
-        <div className="bg-dark-gray rounded-3xl p-5 mt-2 font-bold">
+        <div className="p-5 mt-2 font-bold bg-dark-gray rounded-3xl">
           <div className="text-neutral-500">654,897,987 SDOG Remaining</div>
-          <div className="border border-white rounded-xl flex gap-2 p-1 mt-2">
-            <div className="flex-1 flex items-center px-2">
-              <input type="text" className="w-full outline-none bg-transparent" />
+          <div className="flex gap-2 p-1 mt-2 border border-white rounded-xl">
+            <div className="flex items-center flex-1 px-2">
+              <input type="text" className="w-full bg-transparent outline-none" />
             </div>
             <div className="w-12 rounded-full">
               <img src="/imgs/solana.webp" alt="" className="w-12 rounded-full" />
             </div>
           </div>
           <div className="flex gap-2 mt-2">
-            <button className="bg-neutral-800 w-16 h-6 rounded-lg text-neutral-500">0.1SOL</button>
-            <button className="bg-neutral-800 w-16 h-6 rounded-lg text-neutral-500">0.5SOL</button>
-            <button className="bg-neutral-800 w-16 h-6 rounded-lg text-neutral-500">1SOL</button>
-            <button className="bg-neutral-800 w-16 h-6 rounded-lg text-neutral-500">5SOL</button>
+            <button className="w-16 h-6 rounded-lg bg-neutral-800 text-neutral-500">0.1SOL</button>
+            <button className="w-16 h-6 rounded-lg bg-neutral-800 text-neutral-500">0.5SOL</button>
+            <button className="w-16 h-6 rounded-lg bg-neutral-800 text-neutral-500">1SOL</button>
+            <button className="w-16 h-6 rounded-lg bg-neutral-800 text-neutral-500">5SOL</button>
           </div>
           <div className="mt-5">
             1,500,500 <span className="text-neutral-500">SDOG</span>
           </div>
-          <button className="w-full rounded-lg bg-primary flex items-center justify-center text-black text-sm h-8 transition-all duration-300 font-semibold hover:bg-secondary hover:text-white mt-5">Buy</button>
+          <button className="flex items-center justify-center w-full h-8 mt-5 text-sm font-semibold text-black transition-all duration-300 rounded-lg bg-primary hover:bg-secondary hover:text-white">Buy</button>
         </div>
         <div className="flex items-center justify-center gap-2 mt-5">
           <img src="/imgs/x.webp" alt="" className="" />
           <img src="/imgs/telegram.webp" alt="" className="" />
         </div>
         <div className="mt-5 font-bold">
-          <div className="text-neutral-500 text-lg">Token Distribution</div>
+          <div className="text-lg text-neutral-500">Token Distribution</div>
           <div className="mt-2">
             <div className="flex justify-between">
               <div className="text-neutral-500">1. Liquidity</div>
