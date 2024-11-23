54f9e21ffd3c605e61595b8753b4f906e482e44d
minionlancer <minionlancer28@gmail.com>
2024-11-23 09:49:34 -0800

    fix: update program-id

--- src/core/constants/address.js
+++ src/core/constants/address.js
@@ -1,3 +1,3 @@
-export const SOLANA_COINKICK_PROGRAMID = "AtFfnfFpJDvz6Ti6HUQFS3j2rMjT5hTaam7QKfqG5rxN"; //devnet
+export const SOLANA_COINKICK_PROGRAMID = "BSuGYrEgx9jq1qs52QYXXVh4jWCHJog3hosxVxdaPwrE"; //devnet
 export const FEE_ACCOUNT = "4cASLHHXtomyWCD6muAGeEF17dran74LnXx78LhVfwz3";
 export const LIQUIDITY = "5KjtLUmuFenkLK8ZRsqcC2b7zoLaG1T4rGUeFgkqTGcL";
