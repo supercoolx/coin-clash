84f98a693fbb106c0e28b58983f40243bf35dba3
minionlancer <minionlancer28@gmail.com>
2024-11-24 05:44:11 -0800

    fix: BACKEND_SOCKET

--- src/core/constants/index.js
+++ src/core/constants/index.js
@@ -1,6 +1,6 @@
 export const BACKEND_URI = "https://api.coinkick.fun";
 // export const BACKEND_URI = "http://localhost:3000";
-export const BACKEND_SOCKET = "ws://api.coinkick.fun";
+export const BACKEND_SOCKET = "https://api.coinkick.fun";
 // export const BACKEND_SOCKET = "ws://localhost:3000";
 
 //export const SOLANA_API_URL = "https://api.mainnet-beta.solana.com"
